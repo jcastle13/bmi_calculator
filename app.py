2cbcffa72c6c6b35f3995c6c3848b214aa049c52
Jason Castillo <jcastle13@jasons-mbp.attlocal.net>
2018-02-04 15:59:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-
 from flask import Flask, request, render_template
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def rootpage():
-    weight = ''
-    height = ''
+    weight = 0.0
+    height = 0.0
     bmi = 0.0
     if request.method == 'POST' and 'userweight' in request.form:
         weight = request.form.get('userweight')
         height = request.form.get('userheight')
-        print("Height:", height)
+        #print("Height:", height)
         '''Check if height is a valid number (i.e. floating value)'''
         if isfloat(height):
             if isfloat(weight):
@@ -28,7 +26,7 @@ def rootpage():
             #bmi = "Cannot calculate BMI"
 
 
-        print("Update height:", height)
+        #print("Update height:", height)
     return render_template("index.html", weight = weight, height = height, bmi = bmi)
 
 ''' Function to detect if a string is a valid floating value'''
@@ -40,7 +38,7 @@ def isfloat(value):
         return False
 
 
-
-app.run()
+if __name__ == "__main__":
+    app.run()
 
 
